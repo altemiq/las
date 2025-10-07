14611d183cd88c792d14f8d4cc5ad4ec04989ab6
Ross King <rsking@gmail.com>
2025-10-07 12:04:36 +1100

    ✨ switch to incremental source generation

--- src/IO.Las.CodeGeneration/SourceGenerator.cs
+++ src/IO.Las.CodeGeneration/SourceGenerator.cs
@@ -12,42 +12,78 @@ using Microsoft.CodeAnalysis.CSharp;
 /// <summary>
 /// The LAS source generators.
 /// </summary>
-[Generator]
-public class SourceGenerator : ISourceGenerator
+[Generator(LanguageNames.CSharp)]
+public class SourceGenerator : IIncrementalGenerator
 {
     /// <inheritdoc/>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1035:Do not use APIs banned for analyzers", Justification = "Checked, this is not applicable as an incremental source generator")]
-    public void Execute(GeneratorExecutionContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var modelsCsv = context.AdditionalFiles.Single(static file => Path.GetFileNameWithoutExtension(file.Path) is "models" && Path.GetExtension(file.Path) is ".csv");
-        var platformsCsv = context.AdditionalFiles.Single(static file => Path.GetFileNameWithoutExtension(file.Path) is "platforms" && Path.GetExtension(file.Path) is ".csv");
-
-        if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.Rootnamespace", out var rootNamespace))
+#if DEBUG_SOURCE_GENERATOR
+        if (!System.Diagnostics.Debugger.IsAttached)
         {
-            rootNamespace = typeof(SourceGenerator).Namespace?.Replace(".CodeGeneration", string.Empty) ?? string.Empty;
+            System.Diagnostics.Debugger.Launch();
         }
+#endif
+        var rootNamespaceAndExcludeFromCodeCoverage = context.AnalyzerConfigOptionsProvider.Select(static (options, _) => GetRootNamespace(options))
+            .Combine(context.CompilationProvider.Select(static (compilation, _) => compilation.GetTypeByMetadataName("System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute") is not null));
 
-        var excludeFromCodeCoverage = context.Compilation.GetTypeByMetadataName("System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute") is not null;
+        var platformCsv = context.AdditionalTextsProvider
+            .Where(static file => Path.GetFileNameWithoutExtension(file.Path) is "platforms" && Path.GetExtension(file.Path) is ".csv")
+            .Combine(rootNamespaceAndExcludeFromCodeCoverage);
 
-        var @namespace = GetNamespace(rootNamespace);
-
-        var (ids, types) = Platforms.GetIdsAndTypes(platformsCsv);
-        context.AddSource("PlatformId.cs", GetSourceText(Platforms.CreatePlatformIds(@namespace, ids, excludeFromCodeCoverage)));
-        context.AddSource("PlatformType.cs", GetSourceText(Platforms.CreatePlatformTypes(@namespace, types, excludeFromCodeCoverage)));
-
-        var platforms = Platforms.GetPlatforms(platformsCsv);
-        context.AddSource($"{nameof(Platforms)}.cs", GetSourceText(Platforms.CreatePlatforms(@namespace, platforms)));
-        context.AddSource("Platform.Parse.cs", GetSourceText(Platforms.CreatePlatformParse(@namespace, platforms, excludeFromCodeCoverage)));
-
-        context.AddSource("Brand.cs", GetSourceText(Models.CreateBrands(@namespace, Models.GetBrands(modelsCsv), excludeFromCodeCoverage)));
-
-        var models = Models.GetModels(modelsCsv);
-        context.AddSource($"{nameof(Models)}.cs", GetSourceText(Models.CreateModels(@namespace, models)));
-        context.AddSource("Model.Parse.cs", GetSourceText(Models.CreateModelsParse(@namespace, models, excludeFromCodeCoverage)));
+        var modelCsv = context.AdditionalTextsProvider
+            .Where(static file => Path.GetFileNameWithoutExtension(file.Path) is "models" && Path.GetExtension(file.Path) is ".csv")
+            .Combine(rootNamespaceAndExcludeFromCodeCoverage);
 
 #if LAS1_2_OR_GREATER
-        context.AddSource($"{nameof(System.Drawing.Color)}.cs", GetSourceText(Colors.CreateKnownColors(@namespace)));
+        var color = context.CompilationProvider
+            .Combine(rootNamespaceAndExcludeFromCodeCoverage)
+            .Select(static (c, _) => c.Left.GetTypeByMetadataName($"{c.Right.Left}.{nameof(System.Drawing.Color)}"))
+            .Combine(rootNamespaceAndExcludeFromCodeCoverage);
 #endif
+
+        context.RegisterSourceOutput(platformCsv, static (context, platformCsvWithNamespace) =>
+        {
+            var platformCsv = platformCsvWithNamespace.Left;
+            var (ids, types) = Platforms.GetIdsAndTypes(platformCsv);
+            var @namespace = GetNamespace(platformCsvWithNamespace.Right.Left);
+            var excludeFromCodeCoverage = platformCsvWithNamespace.Right.Right;
+            context.AddSource("PlatformId.cs", GetSourceText(Platforms.CreatePlatformIds(@namespace, ids, excludeFromCodeCoverage)));
+            context.AddSource("PlatformType.cs", GetSourceText(Platforms.CreatePlatformTypes(@namespace, types, excludeFromCodeCoverage)));
+
+            var platforms = Platforms.GetPlatforms(platformCsv);
+            context.AddSource($"{nameof(Platforms)}.cs", GetSourceText(Platforms.CreatePlatforms(@namespace, platforms)));
+            context.AddSource("Platform.Parse.cs", GetSourceText(Platforms.CreatePlatformParse(@namespace, platforms, excludeFromCodeCoverage)));
+        });
+
+        context.RegisterSourceOutput(modelCsv, static (context, modelCsvWithNamespace) =>
+        {
+            var modelCsv = modelCsvWithNamespace.Left;
+            var models = Models.GetModels(modelCsv);
+            var @namespace = GetNamespace(modelCsvWithNamespace.Right.Left);
+            var excludeFromCodeCoverage = modelCsvWithNamespace.Right.Right;
+
+            context.AddSource("Brand.cs", GetSourceText(Models.CreateBrands(@namespace, Models.GetBrands(modelCsv), excludeFromCodeCoverage)));
+            context.AddSource($"{nameof(Models)}.cs", GetSourceText(Models.CreateModels(@namespace, models)));
+            context.AddSource("Model.Parse.cs", GetSourceText(Models.CreateModelsParse(@namespace, models, excludeFromCodeCoverage)));
+        });
+
+#if LAS1_2_OR_GREATER
+        context.RegisterSourceOutput(color, static (context, colorWithNamespace) =>
+        {
+            if (colorWithNamespace.Left is { } color)
+            {
+                context.AddSource($"{color.Name}.Known.cs", GetSourceText(Colors.CreateKnownColors(GetNamespace(colorWithNamespace.Right.Left))));
+            }
+        });
+#endif
+
+        static string GetRootNamespace(Microsoft.CodeAnalysis.Diagnostics.AnalyzerConfigOptionsProvider options)
+        {
+            return options.GlobalOptions.TryGetValue("build_property.rootnamespace", out var rootNamespace)
+                ? rootNamespace
+                : typeof(SourceGenerator).Namespace?.Replace(".CodeGeneration", string.Empty) ?? string.Empty;
+        }
 
         static Microsoft.CodeAnalysis.CSharp.Syntax.BaseNamespaceDeclarationSyntax GetNamespace(string rootNamespace)
         {
@@ -58,18 +94,5 @@ public class SourceGenerator : ISourceGenerator
         {
             return compilationUnitSyntax.NormalizeWhitespace().GetText(System.Text.Encoding.UTF8);
         }
-    }
-
-    /// <inheritdoc/>
-    public void Initialize(GeneratorInitializationContext context)
-    {
-#if DEBUG_SOURCE_GENERATOR
-        if (!System.Diagnostics.Debugger.IsAttached)
-        {
-            System.Diagnostics.Debugger.Launch();
-        }
-#else
-        // This must be implemented
-#endif
     }
 }
