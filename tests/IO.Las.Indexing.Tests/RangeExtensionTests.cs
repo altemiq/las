c5ff0cb187ba0ccf4779ebf57a908c2eee6ad862
Ross King <rsking@gmail.com>
2026-05-07 06:42:33 +1000

    🐛 avoid `uint` overflow in `RangeExtensions.GetIndexes` at `uint.MaxValue`

---
+++ tests/IO.Las.Indexing.Tests/RangeExtensionTests.cs
@@ -0,0 +1,11 @@
+namespace Altemiq.IO.Las.Indexing;
+
+public class RangeExtensionTests
+{
+    [Test]
+    public async Task GetIndexesWithMaxValue()
+    {
+        var indexes = new Range(new(uint.MaxValue - 1), new(uint.MaxValue));
+        await Assert.That(indexes.GetIndexes()).Count().IsEqualTo(2);
+    }
+}
